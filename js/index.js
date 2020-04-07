132a0138a9ace095440fbbd89fc76ab8f58791d0
Nathan Cowley <nathancowley12@gmail.com>
2020-04-07 17:02:55 -0600

    almost finished with the assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,88 @@
 // Your code goes here
+
+
+//Mouseover
+
+const changeNav = document.querySelector('.main-navigation');
+
+changeNav.addEventListener('mouseover', (event) => {
+    
+    event.target.style.color = 'gold';
+});
+
+//Mouseout
+
+changeNav.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'black';
+})
+
+
+//Welcome Alert
+
+const button = document.querySelector('.destination .btn');
+    button.addEventListener('click', (event) => {
+    alert('Lets get YOU Signed up!!')
+})
+
+//change background color of the footer on click
+
+const foot = document.querySelector('.footer')
+foot.addEventListener('click', (event) => {
+    if(event.target.style.backgroundColor === 'grey'){
+    event.target.style.backgroundColor = '#FFEBCD'
+    } else {
+    event.target.style.backgroundColor = 'grey'
+    }
+})
+
+
+//Alert message when page Loads
+
+// window.addEventListener('load', (event) => {
+//     alert("HELLO GOOD PEOPLE!! THANKS FOR VISITING THIS WEBPAGE! TRY CLICKING SIGN UP!")
+// });
+
+//KeyDown
+
+const bodyColor = document.querySelector('body');
+    bodyColor.addEventListener('keydown', (event) => {
+    bodyColor.style.backgroundColor = 'orange';
+});
+
+
+//Wheel, change color to tomato while you scroll
+
+const bodyColor2 = document.querySelector('body');
+bodyColor2.addEventListener('wheel', (event) => {
+    bodyColor2.style.backgroundColor = 'tomato';
+});
+
+//Double Click
+
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('dblclick', () => {
+    if (logoHeading.textContent === 'Fun Bus'){
+        logoHeading.textContent = 'Bus Fun';
+    } else {
+        logoHeading.textContent = 'Fun Bus'
+    }
+});
+
+
+//click border
+
+const imgBorder = document.getElementsByTagName('img');
+imgBorder.addEventListener('click', (event) => {
+    imgBorder.style.border = '5px dotted yellow';
+})
+
+
+
+
+
+
+
+
+
+
+
